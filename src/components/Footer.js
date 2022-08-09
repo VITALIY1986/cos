62e94a0437b6dafeffe1464b2287ffbf1bcf0c37
VITALIY1986 <staslego1986@gmail.com>
2022-08-09 16:11:22 +0300

    sdf

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,15 @@ import Image from 'next/image'
 import profilePic from '../../public/logo.png'
 const Footer = () => (
 	<div className="footer bg-grey_cos text-futer px-6 py-6">
-		<div className="flex justify-between ">
+		<div className="flex flex-col items-center">
+		
+			<div className="w-24">
+	
+						<Image  src={profilePic}  />
+						</div>
 		
 			
-		<Link href="/">
-						<Image  src={profilePic}  height={10} width={70}/>
-					</Link>  
-		
-			
-			<div className="flex ">
+			<div className="flex mt-3">
 			<div className="bg-grey_cos p-3 rounded-full mx-1 shadow-xl"><Facebook/></div><div className="bg-bg-grey_cos  p-3 rounded-full mx-1 shadow-xl"><Instagram/></div>
 			</div>
 			</div>
